81ab395da68e900d370f1972cbf147980182e879
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-09 18:34:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
-  array.collect do |number|
+  new_numbers = []
+  array.each do |number|
     number = number **= 2
+    new_numbers.push(number)
   end
+  new_numbers
 end
